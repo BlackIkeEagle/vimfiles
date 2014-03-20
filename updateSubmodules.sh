3f2c869735900f02d3c2e7d3c27e95277694defe
BlackEagle <ike.devolder@gmail.com>
2014-03-20 10:29:51 +0100

    updateSubmodules :: some improvements
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -5,10 +5,13 @@ for folder in $(find vimfiles/bundle -mindepth 1 -maxdepth 1 -type d); do
 	(cd $folder && git checkout master && git pull)
 done
 
-for submodule in $(git status | grep modified | sed 's/.*\(vimfiles\/bundle\/[A-Za-z]*\) .*/\1/'); do
-	echo $submodule
-	submodulename=${submodule##*/}
-	echo $submodulename
-	git add $submodule
-	git commit -sm "plugin : updated $submodulename"
+IFS=$'\n'
+for submodule in $(git status | grep modified | sed 's/.*\(vimfiles\/bundle\/[A-Za-z-]*\) .*/\1/'); do
+	if [[ $submodule == vimfiles* ]]; then
+		echo $submodule
+		submodulename=${submodule##*/}
+		echo $submodulename
+		git add $submodule
+		git commit -sm "plugin : updated $submodulename"
+	fi
 done
