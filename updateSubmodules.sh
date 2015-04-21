26c28956aaea790548091a04f463889edffd4ccd
BlackEagle <ike.devolder@gmail.com>
2015-04-21 10:01:38 +0200

    updateSubmodules :: enhanced simplified
    - fix base16 update (no numbers were matched)
    - simplify forloop to include only vimifiles/bundle and avoid a useless
      check that way
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 for folder in $(find vimfiles/bundle -mindepth 1 -maxdepth 1 -type d); do
-	echo $folder
-	(cd $folder && git checkout master && git pull)
+    echo $folder
+    (cd $folder && git checkout master && git pull)
 done
 
 IFS=$'\n'
-for submodule in $(git status | grep modified | sed 's/.*\(vimfiles\/bundle\/[_A-Za-z-]*\) .*/\1/'); do
-	if [[ $submodule == vimfiles* ]]; then
-		echo $submodule
-		submodulename=${submodule##*/}
-		echo $submodulename
-		git add $submodule
-		git commit -sm "plugin : updated $submodulename"
-	fi
+for submodule in $(git status | grep modified | grep -i 'vimfiles/bundle' | sed 's/.*\(vimfiles\/bundle\/[_A-Za-z0-9-]*\) .*/\1/'); do
+    echo $submodule
+    submodulename=${submodule##*/}
+    echo $submodulename
+    git add $submodule
+    git commit -sm "plugin : updated $submodulename"
 done
