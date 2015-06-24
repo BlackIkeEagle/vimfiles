d2ae01a620c9f7ad2fdf518f1f75b574fcb4da52
BlackEagle <ike.devolder@gmail.com>
2015-06-24 12:00:53 +0200

    updateSubmodules :: git submodule foreach, thx @WouterSioen
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-for folder in $(find vimfiles/bundle -mindepth 1 -maxdepth 1 -type d); do
-    echo $folder
-    (cd $folder && git checkout master && git pull)
-done
+git submodule foreach 'git checkout master && git pull'
 
 IFS=$'\n'
 for submodule in $(git status | grep modified | grep -i 'vimfiles/bundle' | sed 's/.*\(vimfiles\/bundle\/[_A-Za-z0-9-]*\) .*/\1/'); do
